166acf12a1b699f1ba794052779ffb59e6aae975
Cole DeVries <colemandevries@gmail.com>
2023-01-31 04:58:32 -0500

    just formatting

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 resource "aws_opensearch_domain_policy" "this" {
   domain_name     = var.domain_name
   access_policies = data.aws_iam_policy_document.this.json
-
 }
 
 data "aws_iam_policy_document" "this" {
